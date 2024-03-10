953d04d339cbf33adcb9b09663f22cc2a2281547
Ayush Sharma <ayushsharma14@gmail.com>
2024-03-10 01:56:18 -0600

    Task 4

--- Tasks/Task 4/main.py
+++ Tasks/Task 4/main.py
@@ -11,7 +11,8 @@ dataset2 = pd.read_table("ts2-ddos-test01.netflow",delimiter='\t')
 
 X_train = dataset.iloc[:,:]
 X_test = dataset2.iloc[:,1:]
-Y_test = dataset2.iloc[:,0:1]
+Y_test = dataset2.iloc[:,:1]
+
 
 # Normalizing training value for better numerical and graphical representation
 ss_train = StandardScaler()
@@ -27,15 +28,17 @@ model.fit(X_train)
 
 # Making predictions
 result = model.predict(X_test)
+
+result[result == 1] = 0
+result[result == -1] = 1
+
 accuracy, precision, recall = [], [], []
 
-print(result)
-'''
 accuracy.append(accuracy_score(result, Y_test))
 precision.append(precision_score(result, Y_test))
 recall.append(recall_score(result, Y_test))
 
-df = pd.DataFrame({"Accuracy": accuracy, "Precision": precision, "Recall": recall}, index = dataset)
+df = pd.DataFrame({"Accuracy": accuracy, "Precision": precision, "Recall": recall})
 print(df)
 
 # Plotting the graph
@@ -44,4 +47,3 @@ plt.legend(loc = 1)
 plt.xticks(rotation=0, ha='right')
 plt.tight_layout()
 plt.show()
-'''
